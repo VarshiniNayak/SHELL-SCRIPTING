0675132808dced03b64838d773d28845a479be83
VarshiniNayak <155127222+VarshiniNayak@users.noreply.github.com>
2024-03-19 16:43:54 +0530

    Update forloop.sh

--- forloop.sh
+++ forloop.sh
@@ -1,3 +1,5 @@
+TO PRINT 1 TO 10
+
 #!/bin/bash
 
 for i in {1..10}
@@ -5,3 +7,40 @@ do
         echo $i
 done
 
+TO PRINT 1 TO 10 BY PAUSING EXECUTION WITH FEW SECONDS
+
+#!/bin/bash
+
+for i in {1..10}
+do
+        echo $i
+        sleep 3
+done
+
+TO PRINT ODD NUMBERS (AFTER 2 INTERVAL)
+
+#!/bin/bash
+
+for i in {1..10..2}
+do
+        echo $i
+done
+
+TO PRINT EVEN NUMBERS (AFTER 2 INTERVAL)
+
+#!/bin/bash
+
+for i in {0..10..2}
+do
+        echo $i
+done
+
+TO PRINT TABLES
+
+#!/bin/bash
+
+number=10
+for i in {1..10}
+do
+        echo "$number * $i = $((number * i))"
+done
